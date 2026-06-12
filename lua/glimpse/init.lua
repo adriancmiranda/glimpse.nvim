cbf49a446c38ab3881fca8da2a335d5a8293c7c4
adriancmiranda <adriancmiranda@users.noreply.github.com>
2026-06-12 17:03:23 -0300

    fix: normalize lir integration config to accept boolean or table

--- lua/glimpse/init.lua
+++ lua/glimpse/init.lua
@@ -142,6 +142,7 @@
 ---@field oil? GlimpseOilConfig Keymaps in Oil.nvim (default: enabled)
 ---@field neotree? {enable?:boolean, auto_preview?:boolean} NeoTree integration config
 ---@field telescope? GlimpseTelescopeConfig Preview in Telescope (default: enabled)
+---@field lir? boolean|{enable?:boolean} lir.nvim integration (default: disabled)
 
 local detect = require('glimpse.detect')
 local safety = require('glimpse.safety')
@@ -412,6 +413,7 @@ function M.setup(opts)
 	config = vim.tbl_deep_extend('force', config, opts or {})
 	config.integrations.oil = normalize_oil_config(config.integrations.oil)
 	config.integrations.telescope = normalize_telescope_config(config.integrations.telescope)
+	config.integrations.lir = normalize_telescope_config(config.integrations.lir)
 	kind_cache = {}
 	kind_cache_revision = kind_cache_revision + 1
 	setup_commands()
@@ -452,7 +454,7 @@ function M.setup(opts)
 			require('glimpse.cache').cleanup(config.cache.dir, config.cache.max_age_days)
 		end, 0)
 	end
-	if config.integrations.lir then
+	if config.integrations.lir.enable ~= false then
 		require('glimpse.integrations.lir').setup()
 	end
 end
